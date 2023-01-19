2b37ee775890be9b16ea850ea1b5c9fdc8564d18
erickleclerc <107086802+erickleclerc@users.noreply.github.com>
2023-01-19 14:26:09 -0800

    InputField Exercise Name and Age

--- Assets/GuessingGame/GuessingGame.cs
+++ Assets/GuessingGame/GuessingGame.cs
@@ -1,14 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GuessingGame : MonoBehaviour
 {
+    public TextMeshProUGUI consoleTalking;
+    public TMP_InputField userInput;
+    
 
     public void MyFunction()
     {
         Debug.Log("Pick a number");
+
+        consoleTalking.text = "Pick an Number";
     }
+
+
+    public void GuessingTime()
+    {
+        consoleTalking.text = "Guess a number between 1 and ..." + userInput.text;
+
+        userInput.text = "";
+
+
+    }
+
 
     // Start is called before the first frame update
     void Start()
